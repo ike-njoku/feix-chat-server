20899ddfd6c0508d65f363122fe7d9f3dcbc7cc2
ike-Njoku <ikenjokudc@gmail.com>
2021-12-29 17:56:31 +0100

    deployable

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const PORT = process.env.PORT || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
@@ -8,6 +8,9 @@ async function bootstrap() {
       'http://localhost:4200', 'http://localhost:4201','https://fenixsms.herokuapp.com'
     ]}
   );
-  await app.listen(3000);
+  await app.listen(PORT).then(() => {
+    console.log(`Fenix SMS chat server is running on port ${PORT}`)
+  })
+  .catch((error: any) => console.log(error))
 }
 bootstrap();
